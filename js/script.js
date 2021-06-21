f906d28727e3dfeedfb81323e962ee32306eda08
DennisNjeru9 <dennis.njeri@student.moringaschool.com>
2021-06-21 15:43:25 +0300

    add jQuery function for showing message when a user hovers over the what we do logos

---
+++ js/script.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+  $(".design").hover(function(){
+    $("#design").toggle();
+  });
+
+  $(".development").hover(function(){
+    $("#development").fadeToggle();
+  });
+
+  $(".product-development").hover(function(){
+    $("#product-development").fadeToggle();
+  });
+
+  event.preventDefault();
+});
